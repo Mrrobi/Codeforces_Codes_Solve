f3a0feaa96bfc1c7c52e06bffaa42a0be0df4ad5
Md Robiuddin Robi <mrrobi040@gmail.com>
2018-02-08 19:33:32 +0600

    up

---
+++ 499B.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string mini(string x,string y){
+    if(x.length()<y.length())return x;
+    else return y;
+}
+int main() {
+    pair<string,string>s1[3005];
+    string s2[3005];
+    int n,m;
+    cin>>n>>m;
+    for(int i=0;i<m;i++){
+        cin>>s1[i].first>>s1[i].second;
+    }
+    for(int i=0;i<n;i++){
+        cin>>s2[i];
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(s2[i]==s1[j].first||s2[i]==s1[j].second){
+                cout<<mini(s1[j].first,s1[j].second)<<" ";
+            }
+        }
+    }
+    puts("");
+    return 0;
+}
